51b22cdb6eae948c8cb9394fbafc93c39d662898
redscarf <13183691+a239979@user.noreply.gitee.com>
2024-11-06 21:11:46 +0800

    Third commit
    none

---
+++ src/components/UserInfo/AuthorList.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="author-list">
+      <h3 class="title">合著作者</h3>
+      <div v-for="(author, index) in authors" :key="index" class="author-item">
+        <img class="avatar" :src="author.avatar" alt="Author Avatar" />
+        <div class="author-info">
+          <p class="author-name">{{ author.name }}</p>
+          <p class="author-university">{{ author.university }}</p>
+        </div>
+        <span class="arrow">➔</span>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "AuthorList",
+    props: {
+      authors: {
+        type: Array,
+        required: true,
+        default: () => [
+          {
+            name: "Xiaotian Hu",
+            university: "Nanchang University",
+            avatar: "path/to/avatar1.jpg"
+          },
+          {
+            name: "Zengqi Huang",
+            university: "Nanchang University",
+            avatar: "path/to/avatar2.jpg"
+          },
+          {
+            name: "Yiwan Chen",
+            university: "Jiangxi Normal University",
+            avatar: "path/to/avatar3.jpg"
+          },
+          {
+            name: "Juan Long",
+            university: "Nanchang University",
+            avatar: "path/to/avatar4.jpg"
+          }
+        ]
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .author-list {
+    width: 100%;
+    padding: 10px;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  }
+  
+  .title {
+    font-size: 16px;
+    font-weight: bold;
+    color: #333;
+    margin-bottom: 10px;
+  }
+  
+  .author-item {
+    display: flex;
+    align-items: center;
+    padding: 10px 0;
+    border-bottom: 1px solid #f0f0f0;
+  }
+  
+  .author-item:last-child {
+    border-bottom: none;
+  }
+  
+  .avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+  
+  .author-info {
+    flex: 1;
+  }
+  
+  .author-name {
+    font-size: 14px;
+    font-weight: bold;
+    color: #333;
+  }
+  
+  .author-university {
+    font-size: 12px;
+    color: #888;
+  }
+  
+  .arrow {
+    font-size: 18px;
+    color: #888;
+  }
+  </style>
+  
--- src/pages/userInfo.vue
+++ src/pages/userInfo.vue
@@ -7,20 +7,38 @@
         :researchAreas="user.researchAreas"
         @editProfile="handleEditProfile"
       />
-      <Tabs :tabs="['发表文献','数据分析']" @changeTab="handleTabChange" />
+      <div class="detail">
+        <div class="pagetabs">
+              <Tabs :tabs="['发表文献','数据分析']" @changeTab="handleTabChange" 
+              />
+              <div class="tabdetail">
+                <div v-if="tabs.activeTab === 0">
+                <p>34</p>
+                </div>
+                <div v-if="tabs.activeTab === 1">
+                <p>12</p>
+                </div>
+              </div>
+        </div>
+        <div class="pagewriters">
+          <AuthorList :authors="authorlist.authorData" />
+        </div>
+      </div>
     </div>
   </template>
   
   <script>
-  import avatarImage from "../assets/example_avatar.png";
+import avatarImage from "../assets/example_avatar.png";
+import default_pic from "../assets/default.png";
+import AuthorList from "../components/UserInfo/AuthorList.vue";
 import ProfileHeader from "../components/UserInfo/ProfileHeader.vue";
 import Tabs from "../components/UserInfo/Tabs.vue";
-
   export default {
     name: "Index",
     components: {
       ProfileHeader,
       Tabs,
+      AuthorList,
     },
     data() {
       return {
@@ -33,6 +51,30 @@ import Tabs from "../components/UserInfo/Tabs.vue";
         tabs:{
           activeTab:0,
         },
+        authorlist:{
+          authorData: [
+        {
+          name: "Xiaotian Hu",
+          university: "Nanchang University",
+          avatar: default_pic
+        },
+        {
+          name: "Zengqi Huang",
+          university: "Nanchang University",
+          avatar: default_pic
+        },
+        {
+          name: "Yiwan Chen",
+          university: "Jiangxi Normal University",
+          avatar: default_pic
+        },
+        {
+          name: "Juan Long",
+          university: "Nanchang University",
+          avatar: default_pic
+        }
+      ]
+        }
       };
     },
     methods: {
@@ -40,7 +82,7 @@ import Tabs from "../components/UserInfo/Tabs.vue";
         console.log("Edit profile clicked");
       },
       handleTabChange(index){
-        this.activeTab=index;
+        this.tabs.activeTab = index;
       },
     },
   };
@@ -49,6 +91,23 @@ import Tabs from "../components/UserInfo/Tabs.vue";
   <style scoped>
   .page {
     padding: 20px;
+    background-color: aliceblue;
+    width: 100vw;
+    height: 100%;
+  }
+  .tabdetail{
+    height: 70vh;
+    background-color: white;
+    width: 60vw;
+  }
+  .detail{
+    display: flex;
+    gap: 30px;
+    align-items: stretch;
+  }
+  .pagewriters{
+    background-color: white;
+    width: 40vw;
   }
   </style>
   
