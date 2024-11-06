5f4e066580d1a12e35b67ba9627208bb8a9a0662
涂值旗 <22371449>
2024-11-06 20:59:26 +0800

    article init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup>
-
+import Article from './components/Article.vue';
 </script>
 
 <template>
+<div id="app">
+    <!-- <header>
+      <nav>
+        <router-link to="/article">Article</router-link>
+      </nav>
+    </header> -->
 
+    <!-- 渲染页面内容 -->
+    <router-view />
+  </div>
 </template>
 
 <style scoped>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,36 @@
 import component from 'element-plus/es/components/tree-select/src/tree-select-option.mjs'
 import {
     createRouter,
-    createWebHashHistory
+    createWebHistory
 } from 'vue-router'
 
 import Index from '~/pages/index.vue'
+import Article from '../components/Article.vue'
 
-
-const routes = [{
+const routes = [
+    {
     path: "/",
-    component:Index
+    component: Index
+},
+{
+    path: "/article",
+    component: Article
 }]
 
 
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
+    history: createWebHistory(),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        // 如果目标路由包含哈希，则滚动到指定元素
+        if (to.hash) {
+          return {
+            el: to.hash,  // 滚动到目标元素
+            behavior: 'smooth',  // 平滑滚动
+          }
+        }
+      }
 })
 
 export default router;
