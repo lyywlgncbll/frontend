0b668413419153363e20dbb5222a5992ff4401e7
BUAA-bond <2401829463@qq.com>
2024-11-06 22:17:30 +0800

    侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-
+import Nav from './components/Nav.vue';
+import index from './pages/index.vue';
 </script>
 
 <template>
-
+    <Nav></Nav>
+    <index></index>
 </template>
 
 <style scoped>
-
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,46 @@
-<template>
-    <div>
+<script setup>
+import { ref } from 'vue';
+import searchBar from '../components/searchBar.vue';
+const num = ref(0);
 
+</script>
+
+<template>
+    <div id="search-body">
+
+        <div id="mid">
+            <div class="left-bar">
+                <searchBar></searchBar>
+            </div>
+            <div class="main">
+
+            </div>
+        </div>
     </div>
+
 </template>
+
+<style scoped>
+#search-body {
+    margin: 0 50px;
+}
+
+#mid{
+    height: 100vh;
+    display: flex;
+    margin: 30px auto;
+}
+
+
+.left-bar{
+    width: 20%;
+    height: 90%;
+}
+
+.main{
+    border: 1px solid black;
+    width: 75%;
+    height: 100%;
+    margin: 0 auto;
+}
+</style>
