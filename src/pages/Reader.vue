d4e1e62d303dd3d075be7594de9127f5d810314f
王玮宁 <22371548>
2024-11-13 21:28:07 +0800

    成功渲染pdf到浏览器

--- src/pages/Reader.vue
+++ src/pages/Reader.vue
@@ -1,21 +1,54 @@
 <script setup>
 import * as pdfjs from "pdfjs-dist";
-const pdfPath = "/src/assets/test/test.pdf"
+pdfjs.GlobalWorkerOptions.workerSrc = './public/pdf.worker.mjs'
+const pdfPath = '/test/test.pdf'
 const loadingTask = pdfjs.getDocument(pdfPath)
 loadingTask.promise
-  .then(function (doc) {
-    const numPages = doc.numPages
-    console.log("numPages: " + numPages)
+  .then(function (pdf) {
+    console.log("success loaded pdf")
+    var pageNumber = 1
+    pdf.getPage(pageNumber).then(function (page) {
+      console.log("success loaded page")
+      
+
+      var viewport = page.getViewport({scale: 1})
+      var scale = window.innerHeight/viewport.height
+      console.log("scale: ", scale)
+      viewport = page.getViewport({scale: scale})
+
+      var canvas = document.getElementById("testCanvas")
+      var context = canvas.getContext('2d')
+      // console.log(viewport.height, window.innerHeight)
+      canvas.height = viewport.height
+      canvas.width = viewport.width
+
+      page.render({
+        canvasContext: context,
+        viewport: viewport,
+      }).promise.then(function () {
+        console.log('success rendered page')
+      })
+    })
   })
 
 </script>
 
 <template>
-  <p>reader</p>
+  <div id="pdfView">
+    <canvas id="testCanvas"></canvas>
+  </div>
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+#pdfView {
+  position: relative;
+}
+#testCanvas {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
 }
 </style>
