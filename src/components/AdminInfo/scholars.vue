d2024c6d2d7f15762c429190e8decf1d713a0d34
hid_3d9zw4dqyzhg92d/hid_3d9zw4dqyzhg92d <14192733+off-fu@user.noreply.gitee.com>
2024-12-09 22:40:25 +0800

    修改图表获取

--- src/components/AdminInfo/scholars.vue
+++ src/components/AdminInfo/scholars.vue
@@ -71,7 +71,7 @@ export default{
               id: item.user.id,
               name: item.authorName || '未知',
               mail: item.user.mail || '无邮箱',
-              institution: '未知机构',
+              institution: item.org ||'未知机构',
               createTime: this.formatDate(item.registerDate) || '未知日期',
               articleCount: item.publicationsCount || 0
             }));
