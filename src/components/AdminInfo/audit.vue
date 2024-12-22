a00b0f694b9365a23aecbc93614f574793d88156
hid_3d9zw4dqyzhg92d/hid_3d9zw4dqyzhg92d <14192733+off-fu@user.noreply.gitee.com>
2024-12-22 15:59:18 +0800

    添加认领申请内容显示

--- src/components/AdminInfo/audit.vue
+++ src/components/AdminInfo/audit.vue
@@ -31,7 +31,7 @@ export default {
       tableData: [],
       currentPage: 1,
       totalPage: 1,
-      pageSize: 6,
+      pageSize: 5,
       filterStatus: "pending_only",
     };
   },
@@ -41,14 +41,30 @@ export default {
     pageComponent
   },
   created() {
+    const savedState = sessionStorage.getItem('pageState');
+    if (savedState) {
+      const parsedState = JSON.parse(savedState);
+      this.currentPage = parsedState.currentPage || this.currentPage;
+      this.filterStatus = parsedState.filterStatus || this.filterStatus;
+    }
     this.getClaim();
   },
+  beforeRouteLeave(to, from, next) {
+    this.saveState();
+    next();
+  },
   methods: {
+    saveState(){
+      const state = {
+        currentPage: this.currentPage,
+        filterStatus: this.filterStatus
+      };
+      sessionStorage.setItem('pageState', JSON.stringify(state));
+    },
     getClaim() {
       try {
         axios.get(GETCLAIMALL_API, {
           params: {
-            keyword: this.keyword,
             pageSize: this.pageSize,
             page: this.currentPage,
             queryMode: this.filterStatus
@@ -199,7 +215,7 @@ export default {
 }
 
 .claimTable-pos {
-  margin: 40px 0px;
+  margin: 20px 0px;
 }
 
 .pageComponent {
